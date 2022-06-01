52b9817c3b54030c136bf24bdbb87f06c058d686
wangyuanfeng <934830995@qq.com>
2022-06-01 08:58:59 +0800

    special form

---
+++ index.js
@@ -0,0 +1,172 @@
+layui.use(['layer', 'form'], function () {
+  var layer = layui.layer,
+  form = layui.form,
+  $ = layui.jquery
+  form.render()
+
+  // 要保存的对象
+  var setting = {}
+  // 用来设置输入框绑定的数组
+  var inputOrSelectList = ['set', 'query', 'xxx']
+  //  验证数组
+  var validateList = ['set', 'query', 'xxx', 'concatset', 'concatquery']
+  // 拼接字符串的数组，可调整拼接顺序和拼接数量
+  var concatList = ['set', 'query', 'xxx']
+  // 拼接set的字符串数组
+  var setArr = []
+  // 拼接query的字符串数组
+  var queryArr = []
+  // 需要特殊处理的选项
+  var specialParams = ['xxx']
+
+  // 首先绑定
+  bindMultiInput(inputOrSelectList)
+  // 初始化拼接
+  // concatValue(concatList)
+
+  $('#saveJson').on('click', function () {
+
+    if (!$('#type').val()) {
+      layer.msg('请选择type')
+      return false
+    }
+
+    valiMultiInputOrSelect(validateList) && saveJSON(setting, 'setting.json')
+
+  })
+
+  // type选择
+  form.on("select(type)", function(data) {
+    if (data.value === 'set') {
+      $('#queryInput').val('null')
+      $('#setInput').val('')
+    } else if (data.value === 'query') {
+      $('#setInput').val('null')
+      $('#queryInput').val('')
+    } else {
+      $('#setInput').val('')
+      $('#queryInput').val('')
+    }
+    concatValue(concatList)
+    setting.type = data.value
+  })
+
+  // 保存为json文件
+  function saveJSON(data, filename) {
+    if (!data) {
+      alert('保存数据不能为空')
+      return false
+    }
+    if (!filename) {
+      filename = 'myFile.json'
+    }
+    if (typeof data === 'object') {
+      data = JSON.stringify(data, undefined, 2)
+    }
+    var blob = new Blob([data], {type: 'text/json'})
+    // var event = document.createEvent('MouseEvents')
+    var a = document.createElement('a')
+    a.download = filename
+    a.href = window.URL.createObjectURL(blob)
+    a.dataset.downloadurl = ['text/json', a.download, a.href].join(':')
+    // event.initMouseEvent('click', true, false, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
+    // a.dispatchEvent(event)
+    a.click() // 并不是所有浏览器都支持所有元素click方法，只有button、input所有浏览器支持click
+  }
+
+  // 选择框绑定输入框
+  function bindMultiInput(params) {
+    params.forEach(param => {
+      form.on(`select(${param})`, function (data) {
+        $(`#${param}Input`).length && $(`#${param}Input`).val(data.value)
+        concatValue(concatList)
+      })
+    })
+  }
+
+  // 验证函数
+  function valiMultiInputOrSelect(params) {
+    // 表示有一个验证不通过，则表单不会通过
+    return params.every(param => {
+      var paramInput = $(`#${param}Input`)
+      if (paramInput.length) {
+        if (paramInput.val().trim() === '') {
+          return false
+        } else {
+          setting[param] = paramInput.val() === 'null' ? null : paramInput.val().trim()
+          return true
+        }
+      } else {
+        setting[param] = $(`#${param}Select`).val()
+        return true
+      }
+    })
+  }
+
+  // 拼接字符串
+  function concatValue(params) {
+    setArr = []
+    queryArr = []
+    var type = $('#type').val(),
+      concatSet = $('#concatsetInput'),
+      concatQuery = $('#concatqueryInput')
+    if (type === 'set') {
+      getConcatArr(params, setArr)
+      setArr.push('SET')
+      // 首尾确定
+      setArr.push('REQ')
+      setArr.unshift('ID')
+      // 拼接
+      // $('#concatsetInput').val(setArr.join('_'))
+      // $('#concatqueryInput').val('null')
+      concatSet.val(setArr.join('_'))
+      concatQuery.val('null')
+    } else if (type === 'query') {
+      getConcatArr(params, queryArr)
+      queryArr.push('QRY')
+      // 首尾确定
+      queryArr.push('REQ')
+      queryArr.unshift('ID')
+      // 拼接
+      concatQuery.val(queryArr.join('_'))
+      concatSet.val('null')
+    } else if (type === 'set&query') {
+      getConcatArr(params, setArr, queryArr)
+      queryArr.push('QRY')
+      setArr.push('SET')
+      // 首尾确定
+      queryArr.push('REQ')
+      setArr.push('REQ')
+      queryArr.unshift('ID')
+      setArr.unshift('ID')
+      concatSet.val(setArr.join('_'))
+      concatQuery.val(queryArr.join('_'))
+    }
+  }
+
+  function getConcatArr(params, arr1, arr2 = null) {
+    params.forEach(param => {
+      var paramInput = $(`#${param}Input`)
+      var paramSelect = $(`#${param}Select`)
+      if (paramInput.length && paramInput.val().trim() != 'null') {
+        if (specialParams.includes(param)) {
+          var index = paramInput.val().trim().lastIndexOf('_')
+          paramInput.val().trim() && arr1.push(paramInput.val().trim().toUpperCase().slice(index + 1))
+          arr2 && paramInput.val().trim() && arr2.push(paramInput.val().trim().toUpperCase().slice(index + 1))
+        } else {
+          paramInput.val().trim() && arr1.push(paramInput.val().trim().toUpperCase())
+          arr2 && paramInput.val().trim() && arr2.push(paramInput.val().trim().toUpperCase())
+        }
+      } else if (!paramInput.length && paramSelect.val().trim() != 'null') {
+        if (specialParams.includes(param)) {
+          var index = paramSelect.val().lastIndexOf('_')
+          arr1.push(paramSelect.val().toUpperCase().slice(index + 1))
+          arr2 && arr2.push(paramSelect.val().toUpperCase().slice(index + 1))
+        } else {
+          arr1.push(paramSelect.val().toUpperCase())
+          arr2 && arr2.push(paramSelect.val().toUpperCase())
+        }
+      }
+    })
+  }
+})
